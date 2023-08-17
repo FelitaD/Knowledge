ea3011d6ed8c893c8a5c7abd673be7647f5446ae
Donor <donorfelita@msn.com>
2023-08-17 18:17:55 +0200

    Add kafka code

---
+++ Distributed Systems/Kafka/Code/producer_key.py
@@ -0,0 +1,16 @@
+from kafka import KafkaProducer
+
+
+kafka_producer = KafkaProducer(
+  bootstrap_servers="localhost:9092"
+)
+
+for i in range(1, 30):
+  kafka_producer.send(
+    topic="test",
+    value=f"New message # {i}".encode("utf-8"),
+    # la clef vaudra '0' pour les messages pairs, et '1' pour les messages impairs
+    key=f"{i % 2}".encode("utf-8")
+  )
+
+kafka_producer.flush()
